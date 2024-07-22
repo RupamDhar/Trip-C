3f90f2cd0a896d9cd42e17ef186d18240890dd92
RupamDhar <rupamdhar1000@gmail.com>
2024-07-22 21:36:48 +0530

    Rupam - Fork
    -Fixed UI

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,12 @@ function App() {
 
             {/* About Us */}
             <div className="about-us-wrapper h-fit w-screen box-border flex justify-center">
-                <div className="about-us w-[80%] sm:w-[95%]">
-                    <div className="about-us-header mb-[10px] text-[30px] font-bold text-[#343434] sm:text-[22px] sm:pb-[10px]">About Us</div>
+                <div className="about-us w-[80%] sm-max:w-[95%]">
+                    <div className="about-us-header mb-[10px] text-[30px] font-bold text-[#343434] sm-max:text-[22px] sm-max:pb-[10px]">About Us</div>
                     <div className="about-us-body mb-[20px] text-[#797979]">
                         C is India's leading premium mobility solution provider, covering 79 cities nationwide, founded to revolutionize personal ground transportation. Pioneers of smart mobility, we offer reliable, tech-driven, business class solutions for seamless travel experiences.
                     </div>
-                    <button className='read-more-btn px-[25px] py-[10px] border border-yellow-400 rounded-full text-yellow-400 transition-all duration-[0.15s] ease-in-out sm:w-full hover:bg-yellow-400 hover:text-black'>Read More</button>
+                    <button className='read-more-btn px-[25px] py-[10px] border border-yellow-400 rounded-full text-yellow-400 transition-all duration-[0.15s] ease-in-out sm-max:w-full hover:bg-yellow-400 hover:text-black'>Read More</button>
                 </div>
             </div>
 
